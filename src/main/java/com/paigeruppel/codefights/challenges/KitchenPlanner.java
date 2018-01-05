28ec43f438ab6a752b1828077c4b736442dd8e7d
Paige Ruppel <paige.ruppel@gmail.com>
2018-01-05 14:02:26 -0500

    passing first two test cases

--- src/main/java/com/paigeruppel/codefights/challenges/KitchenPlanner.java
+++ src/main/java/com/paigeruppel/codefights/challenges/KitchenPlanner.java
@@ -7,21 +7,27 @@ import java.util.Map;
 
 public class KitchenPlanner {
     public String[] areYouHungary(String[] chefs, String[] food, int[][] values) {
-        int maxValue = -1;
+        int maxValue = -2;
         int index = 0;
 
         Map<String, String> pairs = new HashMap<>();
+
         for (int j = 0; j < food.length; j++) {
             for (int i = 0; i < values.length; i++) {
-                if (values[i][j] > maxValue) {
+                if (values[i][j] >= maxValue) {
                     index = i;
                     maxValue = values[i][j];
                 }
             }
             pairs.put(chefs[index], food[j]);
         }
-        String[] answer = new String[3];
-        answer[0] = String.valueOf(pairs.entrySet());
+        String[] answer = new String[chefs.length];
+        for (int i = 0; i < answer.length; i++) {
+            String chef = chefs[i];
+            StringBuilder foodForChef = new StringBuilder();
+            foodForChef.append(pairs.getOrDefault(chef, "Nothing"));
+            answer[i] = chef + " - " + foodForChef.toString();
+        }
         return answer;
     }
 }
